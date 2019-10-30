bacab217de58cec44ac0354a59ee9eb9adeda685
MARY <phunmung@gmail.com>
2019-10-30 15:54:16 +0900

    game upload

---
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string map)
+    {
+        // randomize a position
+        var spawnPosition = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
+
+        GameObject player = (GameObject)Resources.Load("SciFiWarriorPBR");
+        // instantiate cube
+        BoltNetwork.Instantiate(player, spawnPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Bolt.EntityBehaviour<ICubeStateCustom>
+{
+    private Camera camera;
+    private CharacterController characterController;
+    private Animator animator;
+    //start()
+    public override void Attached()
+    {
+        //network
+        state.SetTransforms(state.CubeTransform, transform);
+        
+        //player
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        camera = FindObjectOfType<Camera>();
+    }
+
+    //update()
+    public override void SimulateOwner()
+    {
+        var speed = 5f;
+        var movement = Vector3.zero;
+        state.Shot = Input.GetMouseButton(0);
+
+        if (Input.GetKey(KeyCode.W)) { movement.z += 1; }
+        if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
+        if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+        if (Input.GetKey(KeyCode.W)) { movement.z += speed; }
+        if (Input.GetKey(KeyCode.S)) { movement.z -= speed; }
+        if (Input.GetKey(KeyCode.A)) { movement.x -= speed; }
+        if (Input.GetKey(KeyCode.D)) { movement.x += speed; }
+
+        Ray cameraRay = camera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
+        if (movement != Vector3.zero)
+        {
+            transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+        }
+        
+        animator.SetBool("Run", movement != Vector3.zero);
+        animator.SetBool("Shot", state.Shot);
+    }
+}
